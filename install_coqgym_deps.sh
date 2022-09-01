ecdf88798095bd5ca60089a8cacdb163c658b1d2
Tom Reichel <treichel@swarm.cs.umass.edu>
2022-08-31 20:41:53 -0400

    reworked coqgym_deps install script

--- install_coqgym_deps.sh
+++ install_coqgym_deps.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -v $OPAMJOBS ]]
+then
+    OPAMJOBS=16
+fi
+
 if ! command -v ruby &> /dev/null
 then
     # First, install Ruby, as that is for some reason required to build
@@ -10,6 +15,8 @@ then
     ~/.local/ruby-build 3.1.2 ~/.local/
 fi
 
+
+
 git submodule update && git submodule init
 
 # Sync opam state to local
@@ -18,31 +25,50 @@ rsync -av --delete $HOME/.opam.dir/ /tmp/${USER}_dot_opam | tqdm --desc="Reading
 # Create the 8.10 switch
 opam switch create coq-8.10 4.07.1
 eval $(opam env --switch=coq-8.10 --set-switch)
-opam pin add -y coq 8.10.2
-
-# Install dependency packages for 8.10
+opam pin -ny add coq 8.10.2
 opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam repo add psl-opam-repository https://github.com/uds-psl/psl-opam-repository.git
+opam repo add psl-opam https://github.com/uds-psl/psl-opam-repository.git
+
+
+# using pins to specify versions of packages we need, but DELAYING
+# the install of those packages -- running no risk of having to recompile packages
+# due to successive install commands
+
+# coq-cheerios/coq-ext-lib master does not support coq-8.10
+opam pin -ny add coq-cheerios "git+https://github.com/uwplse/cheerios.git#f0c7659c44999c6cfcd484dc3182affc3ff4248a"
+opam pin -ny add coq-ext-lib "git+https://github.com/coq-community/coq-ext-lib.git#506d2985cc540ad7b4a788d9a20f6ec57e75a510"
+opam pin -ny add coq-lin-alg https://github.com/tom-p-reichel/lin-alg-8.10.git
+opam pin -ny add coq-metalib "git+https://github.com/plclub/metalib.git#021bea02c86d80849d9e353eefec3cd165ba3be4"
+opam pin -ny add coq-bellantonicook "git+https://github.com/tom-p-reichel/bellantonicook.git"
+#opam pin -ny add coq-psl-base-library "git+https://github.com/uds-psl/base-library"
+opam pin -ny add menhir 20190626
+
+
+# Install dependency packages for 8.10
 opam install -y coq-serapi \
      coq-struct-tact \
      coq-inf-seq-ext \
-     coq-cheerios \
      coq-verdi \
      coq-smpl \
      coq-int-map \
      coq-pocklington \
      coq-mathcomp-ssreflect coq-mathcomp-bigenough coq-mathcomp-algebra\
      coq-fcsl-pcm \
-     coq-ext-lib \
      coq-simple-io \
      coq-list-string \
      coq-error-handlers \
      coq-function-ninjas \
      coq-algebra \
-     coq-zorns-lemma
+     coq-zorns-lemma \
+     coq-metalib.dev \
+     coq-cheerios \
+     coq-ext-lib \
+     coq-lin-alg \
+     coq-psl-base-library \
+     coq-bellantonicook \
+     menhir
 
-opam pin -y add menhir 20190626
 # coq-equations seems to rely on ocamlfind for it's build, but doesn't
 # list it as a dependency, so opam sometimes tries to install
 # coq-equations before ocamlfind. Splitting this into a separate
@@ -50,21 +76,9 @@ opam pin -y add menhir 20190626
 opam install -y coq-equations \
      coq-metacoq coq-metacoq-checker coq-metacoq-template
 
-# Metalib doesn't install properly through opam unless we use a
-# specific commit.
-(cd coq-projects/metalib && opam install .)
-
-(cd coq-projects/lin-alg && make "$@" && make install)
-
-# Install the psl base-library from source
-mkdir -p deps
-git clone -b coq-8.10 git@github.com:uds-psl/base-library.git deps/base-library
-(cd deps/base-library && make "$@" && make install)
-
-git clone git@github.com:davidnowak/bellantonicook.git deps/bellantonicook
-(cd deps/bellantonicook && make "$@" && make install)
-
 # Create the coq 8.12 switch
+mkdir deps
+# TODO: fix all versions that are downloaded from git like above
 opam switch create coq-8.12 4.07.1
 eval $(opam env --switch=coq-8.12 --set-switch)
 opam pin add -y coq 8.12.2
@@ -84,11 +98,14 @@ git clone git@github.com:uwplse/StructTact.git deps/StructTact
 (cd deps/StructTact && opam install -y . )
 git clone git@github.com:DistributedComponents/InfSeqExt.git deps/InfSeqExt
 (cd deps/InfSeqExt && opam install -y . )
+
 # Cheerios has its own issues
-git clone git@github.com:uwplse/cheerios.git deps/cheerios
-(cd deps/cheerios && opam install -y --ignore-constraints-on=coq . )
+opam pin -ny coq-cheerios "git+https://github.com/uwplse/cheerios.git#9c7f66e57b91f706d70afa8ed99d64ed98ab367d"
+opam install -y --ignore-constraints-on=coq coq-cheerios
+
 (cd coq-projects/verdi && opam install -y --ignore-constraints-on=coq . )
 (cd coq-projects/fcsl-pcm && make "$@" && make install)
 
 # Finally, sync the opam state back to global
 rsync -av --delete /tmp/${USER}_dot_opam/ $HOME/.opam.dir | tqdm --desc="Writing shared opam state" > /dev/null
+
